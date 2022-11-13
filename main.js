b9d837dace1684df0b1bb8ca282e449e37e29444
Prince-919 <86801714+Prince-919@users.noreply.github.com>
2022-11-13 06:10:19 -0800

    Dev Tool AutoMatic Open

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const {app, BrowserWindow} = require('electron'); 
 
+console.log('Main Process');
+
 function createWindow()
 {
     const win = new BrowserWindow({
         width:800,
         height:600,
     })
-    win.loadFile('index.html');
+    win.loadFile('index.html'); // run file 
+
+    // Method use (webContents.openDevTools())
+    // win.webContents.openDevTools();   // open automatic Web dev tool
 }
 
 app.whenReady().then(createWindow);
